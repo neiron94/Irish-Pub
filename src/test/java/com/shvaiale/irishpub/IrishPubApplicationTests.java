6452c30857919f8fc020d3a9d6f5d9d959803d34
Ales Shvaibovich <shvaiale@fel.cvut.cz>
2024-07-26 14:36:04 +0200

    :tada: Create project

---
+++ src/test/java/com/shvaiale/irishpub/IrishPubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shvaiale.irishpub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrishPubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
